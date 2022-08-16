ad119bb14bacad3f1d0f5d98fda3ab550ec7cb7b
Cruzguzman <75910427+Cruzguzman@users.noreply.github.com>
2022-08-16 09:55:12 -0500

    Initial commit

---
+++ src/POO/HOLAMUNDO.java
@@ -0,0 +1,16 @@
+package POO;
+
+import javax.swing.*;
+
+public class HOLAMUNDO {
+    public static void main(String[] args) {
+
+
+      String mensaje= JOptionPane.showInputDialog("Anota tu nombre ");
+
+      JOptionPane.showMessageDialog(null,mensaje);
+
+    }
+
+
+}
